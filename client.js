85ded7eaf020ce634b45ca2e5dbd0ebba7b1654e
MadTofu22 <stutler.tom@gmai..com>
2020-10-01 14:26:41 -0500

    initial commit

---
+++ client.js
@@ -0,0 +1,18 @@
+console.log('client.js loaded');
+
+${doscument}.ready(onReady);
+
+function onReady () {
+    
+    console.log('hello from jquery');    
+}
+
+function hello () {
+    
+    console.log('helooooooo');    
+}
+
+function goodbye () {
+    
+    console.log('goodbyeeeee');   
+}
